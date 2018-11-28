c97d4caa2c77cb11aab86e0b6e5ea5e2c5e6e0d0
Brenden Thornton <thorntonbrenden@gmail.com>
2018-11-28 00:17:19 +0000

    automatically backed up by learn

---
+++ conditionals.rb
@@ -0,0 +1,12 @@
+
+  name = gets.strip
+  
+if name == "Brenden"
+    puts "Hi Brenden!"
+  else if name == "Mariana"
+    puts "Hi Mariana!"
+  else if name == "Noa"
+    puts "Hi Noa!"
+  else
+    puts "Hi Weirdo!"
+end
